eb2079bd388e2f18a71e7bdbb9c7728604833ac1
shivam06131 <sv06131@gamilcom>
2022-04-08 16:35:20 +0530

    left-section-done

--- src/pages/signIn/signInSections/SignInLeft.js
+++ src/pages/signIn/signInSections/SignInLeft.js
@@ -3,8 +3,8 @@ import LeftImg from "../assets/left-section/shutterstock_1614042562.png";
 
 const SignInLeft = () => {
   return (
-    <div>
-      <img className="left-img" src={LeftImg} alt="" srcset="" />
+    <div className="left-img">
+      <img src={LeftImg} alt="" srcset="" />
     </div>
   );
 };
